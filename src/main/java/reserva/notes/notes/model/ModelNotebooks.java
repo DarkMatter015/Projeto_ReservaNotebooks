e657bf0be99fae74449c0d1bb9122903858617aa
Carlos Correia <carlos.correia@sistemafiep.org.br>
2024-03-28 21:43:36 -0300

    notebooks.id estava como "long". Mudei para "Long"

--- src/main/java/reserva/notes/notes/model/ModelNotebooks.java
+++ src/main/java/reserva/notes/notes/model/ModelNotebooks.java
@@ -7,7 +7,7 @@ import jakarta.persistence.*;
 public class ModelNotebooks {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     private int numero;
 
@@ -15,11 +15,11 @@ public class ModelNotebooks {
 
     private Long categoria_id;
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
