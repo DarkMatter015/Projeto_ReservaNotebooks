c75de824773c602e8b8368966a923b65c54d5057
Aluno <alanf.boesing@gmail.com>
2024-04-01 14:55:47 -0300

    Incluido buscar por id e apagar por id em ServiceNotebooks

--- src/main/java/reserva/notes/notes/service/ServiceAgendamentoNotes.java
+++ src/main/java/reserva/notes/notes/service/ServiceAgendamentoNotes.java
@@ -2,11 +2,13 @@ package reserva.notes.notes.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reserva.notes.notes.exception.RegistroNaoEncontradoException;
 import reserva.notes.notes.model.ModelAgendamentoNotes;
 import reserva.notes.notes.model.ModelNotebooks;
 import reserva.notes.notes.repo.RepoAgendamentoNotes;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ServiceAgendamentoNotes {
@@ -20,7 +22,18 @@ public class ServiceAgendamentoNotes {
     public List<ModelAgendamentoNotes> listarAgendamentoNotes(){
         return repoAgendamentoNotes.findAll();
     }
-    public void apagarAgendamentoNotes(ModelAgendamentoNotes agendamentoNotes){
+
+    public ModelAgendamentoNotes buscarAgendamentoNotesPorId(Long id) throws RegistroNaoEncontradoException{
+        Optional<ModelAgendamentoNotes> opt = repoAgendamentoNotes.findById(id);
+        if (opt.isPresent()){
+            return opt.get();
+        }else {
+            throw new RegistroNaoEncontradoException("Agendamento notes com id: " + id + " não existe");
+        }
+    }
+
+    public void apagarAgendamentoNotes(Long id) throws RegistroNaoEncontradoException{
+        ModelAgendamentoNotes agendamentoNotes = buscarAgendamentoNotesPorId(id);
         repoAgendamentoNotes.delete(agendamentoNotes);
     }
 }
