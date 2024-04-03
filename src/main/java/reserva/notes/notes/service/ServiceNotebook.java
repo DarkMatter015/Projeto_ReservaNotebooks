cb219f000de78fad9c1906b43b45c2255320d9ac
Carlos Correia <carlos.correia@sistemafiep.org.br>
2024-04-03 16:43:45 -0300

    ops sobrou um conflito em reponotebook

--- src/main/java/reserva/notes/notes/service/ServiceNotebook.java
+++ src/main/java/reserva/notes/notes/service/ServiceNotebook.java
@@ -4,19 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reserva.notes.notes.exception.RegistroNaoEncontradoException;
 import reserva.notes.notes.model.ModelNotebook;
-<<<<<<< HEAD
+
 import reserva.notes.notes.repo.RepoNotebook;
-=======
-import reserva.notes.notes.repo.RepoNotebooks;
->>>>>>> main
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+
 public class ServiceNotebook {
     @Autowired
-<<<<<<< HEAD
     private RepoNotebook repoNotebook;
 
     public ModelNotebook salvarNotebook(ModelNotebook notebook){
@@ -29,20 +26,7 @@ public class ServiceNotebook {
 
     public ModelNotebook buscarNotebookPorId(Long id)throws RegistroNaoEncontradoException {
         Optional<ModelNotebook> opt = repoNotebook.findById(id);
-=======
-    private RepoNotebooks repoNotebooks;
 
-    public ModelNotebook salvarNotebook(ModelNotebook notebook){
-        return repoNotebooks.save(notebook);
-    }
-
-    public List<ModelNotebook> listarNotebooks(){
-        return repoNotebooks.findAll();
-    }
-
-    public ModelNotebook buscarNotebookPorId(Long id)throws RegistroNaoEncontradoException {
-        Optional<ModelNotebook> opt = repoNotebooks.findById(id);
->>>>>>> main
         if (opt.isPresent()) {
             return opt.get();
         } else {
@@ -52,11 +36,7 @@ public class ServiceNotebook {
 
     public void apagarNotebook(Long id) throws RegistroNaoEncontradoException {
         ModelNotebook notebook = buscarNotebookPorId(id);
-<<<<<<< HEAD
         repoNotebook.delete(notebook);
-=======
-        repoNotebooks.delete(notebook);
->>>>>>> main
     }
 
 }
